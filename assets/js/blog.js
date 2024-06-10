aa9d2936031d60bb7ca51de928c12cfa54ec0001
Rich <richardstrain@gmail.com>
2024-06-10 10:28:56 -0400

    Added Materialize CSS Framework

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,42 +1,51 @@
+// Back History Button Logic
+const backButton = document.querySelector('#goBack');
+backButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  history.back();
+});
+// Dynamic blog card creation
 document.addEventListener('DOMContentLoaded', () => {
   // grab local storage data and display it on the page dynamically
   const blogs = JSON.parse(localStorage.getItem('blogsArray')) || [];
 
   // loop blogs array and create a card for each blog
   for (const blog of blogs) {
-    console.log(
-      `Username: ${blog.username}, Title: ${blog.title}, Content: ${blog.content}`
-    );
+    console.log(`Username: ${blog.username}, Title: ${blog.title}, Content: ${blog.content}`);
+
     // define the card container
     const cardContainer = document.querySelector('#card-container');
 
     // create the blog card and add the class
     const card = document.createElement('div');
-    card.classList.add('blog-card');
+    card.classList.add('card');
 
-    // create header of the blog card
-    const header = document.createElement('div');
-    header.classList.add('blog-header');
+    // create card content and add the class
+    const cardContent = document.createElement('div');
+    cardContent.classList.add('card-content');
 
-    // create the paragraph element for the title
-    const cardTitle = document.createElement('p');
-    cardTitle.textContent = `Title: ${blog.title}`;
+    // create the card title and add the class
+    const cardTitle = document.createElement('span');
+    cardTitle.classList.add('card-title');
+    cardTitle.textContent = `${blog.title}`;
 
-    // create the blog content container
-    const cardBody = document.createElement('div');
-    const cardUsername = document.createElement('p');
-    const cardContent = document.createElement('p');
-    cardBody.classList.add('blog-content');
+    const blogContent = document.createElement('p');
+    blogContent.textContent = `${blog.content}`;
 
-    cardUsername.textContent = `Author: ${blog.username}`;
-    cardContent.textContent = `${blog.content}`;
+    // create card action and add the class
+    const cardAction = document.createElement('div');
+    cardAction.classList.add('card-action');
+
+    // create the card author
+    const cardAuthor = document.createElement('span');
+    cardAuthor.textContent = `Author: ${blog.username}`;
 
     // append child elements to the card
-    header.appendChild(cardTitle); // append the title to the header
-    card.appendChild(header); // append the header to the card
-    cardBody.appendChild(cardUsername); // append the username to the card body
-    cardBody.appendChild(cardContent); // append the content to the card body
-    card.appendChild(cardBody); // append the content to the card
-    cardContainer.appendChild(card); // append the card to the card container
+    cardContent.appendChild(cardTitle);
+    cardContent.appendChild(blogContent);
+    cardAction.appendChild(cardAuthor);
+    card.appendChild(cardContent);
+    card.appendChild(cardAction);
+    cardContainer.appendChild(card);
   }
 });
